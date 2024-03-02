f34ff19943eab900ab203abae092f89e2b7c3485
complay11 <84672664+complay11@users.noreply.github.com>
2024-03-02 14:28:17 +0100

    yes

--- removeTempfile.bat
+++ removeTempfile.bat
@@ -3,5 +3,6 @@ color 0a
 title Removing temporary code
 echo This window removes the temporary file that Bob created to execute the code
 echo You don't have a virus that is doing anything with this command prompt.
+timeout /t 1
 del tempfile.py
 exit
